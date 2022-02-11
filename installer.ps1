660d37b3ebd581ed0b1ac274bd0835cf7ab007ea
MattMatt19 <98746861+MattMatt19@users.noreply.github.com>
2022-02-11 12:39:37 +0100

    Suggestions (#1)
    * refactoring, squashme
    
    * refactor, squashme
    
    * squashme, refactoring
    
    * squashme, wip
    
    Co-authored-by: David Zaltron <david@mach2informatica.it>

--- installer.ps1
+++ installer.ps1
@@ -1,32 +1,25 @@
-##Installer-x for quick and automatic utilities installation and Windows Updates
+# Installer-x for quick and automatic utilities installation and Windows Updates
 
-#just a new line
-Function newLine($nLines) {
-    $count = 0
-    while ( $count -lt $nLines) {
-        write-host "`n"
-        $count++
-    }
-}
+# Dot source functions file (evil)
+.\functions.ps1
 
-#admin req
+# Check admin req
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
 	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $PSCommandArgs" -Verb RunAs
 	Exit
 }
 
-#system check
+# Check OS Version
 if ( [System.Environment]::OSVersion.Version.Build -lt 18363 ) {
-    "ATTENZIONE: you need to update to Windows 10 version 1909 at least ..."
+    "Warning: you need to update to Windows 10 version 1909 at least ..."
     Start-Sleep -s 5
     Exit
 }
 
-Write-Output "Automatic Installation - Personalize it"
- 
-newLine(3)
+Write-Output "### Automatic Installation - Personalize it"
+printNewLine(3)
 
-#net temporary mapping
+# net temporary mapping
 $device = "device to find"
 $rootPath = "\\ip\path"
 
@@ -34,110 +27,71 @@ try {
     New-PSDrive -Name $device -PSProvider "FileSystem" -Root $rootPath
 } catch {
     "$device already mapped"
-    newLine(1)
 }
 
-#winget packages (still don't know how to check if they are already installed before the invoke-webReq #damnit)
-$output=".\DesktopAppInstaller.appxbundle"
-$output2=".\VCLibs.appx"
-$json=Invoke-WebRequest 'https://api.github.com/repos/microsoft/winget-cli/releases/latest' -UseBasicParsing
-$psobj = ConvertFrom-Json $json
-$version=$psobj.tag_name
-Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/$version/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile $output -UseBasicParsing
+# winget packages
+# FIXME: check if packages are already installed before invoking the web request
+$DesktopAppPath = ".\DesktopAppInstaller.appxbundle"
+$wingetRelease = Invoke-WebRequest 'https://api.github.com/repos/microsoft/winget-cli/releases/latest' -UseBasicParsing
+$wingetVersion = (ConvertFrom-Json $wingetRelease).tag_name
+$DesktopAppInstallerUri = "https://github.com/microsoft/winget-cli/releases/download/$wingetVersion/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" 
+Invoke-WebRequest -Uri $DesktopAppInstallerUri -UseBasicParsing -OutFile $DesktopAppPath 
+addPackages($DesktopAppPath)
+
+
+$VCLibsAppPath  = ".\VCLibs.appx"
 $env:PROCESSOR_ARCHITECTURE
-
-#variables for checking if package are already downloaded
-$path64 = "Microsoft.VCLibs.x64.14.00.Desktop.appx"
-$path86 = "Microsoft.VCLibs.x86.14.00.Desktop.appx"
-
 if ([Environment]::Is64BitOperatingSystem) {
-    Invoke-WebRequest -Uri "https://aka.ms/$path64" -OutFile $output2 -UseBasicParsing
+    Invoke-WebRequest -Uri "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx" -OutFile $VCLibsAppPath -UseBasicParsing
 }
 else {
-    Invoke-WebRequest -Uri "https://aka.ms/$path86" -OutFile $output2 -UseBasicParsing
+    Invoke-WebRequest -Uri "https://aka.ms/Microsoft.VCLibs.x86.14.00.Desktop.appx" -OutFile $VCLibsAppPath -UseBasicParsing
 }
+addPackages($VCLibsAppPath) 
 
-#check if packages are already installed
-Function getPackagesVersion ($outputx) {
-    $FileVersion = (Get-ItemProperty -Path $outputx ).VersionInfo.ProductVersion
-    $HighestInstalledVersion = Get-AppxPackage -Name Microsoft.VCLibs* |
-        Sort-Object -Property Version |
-        Select-Object -ExpandProperty Version -Last 1
-    
-    if ($HighestInstalledVersion -eq "") {
-        Add-AppPackage -path $outputx
-        Write-Host "$outputx installed"
-    } else {
-        if ($HighestInstalledVersion -lt $FileVersion ) {
-            Add-AppxPackage -Path $outputx
-        } else {
-            newLine(1)
-            Write-Host "$outputx is updated"
-        }
-    }
-}
 
-getPackagesVersion ($output2)
-getPackagesVersion ($output)
-
-##Utilities Install (code in progress for office options)
+## Utilities Install (WIP)
 #uni-variables
-$installationType
+$deployType
 $suppCountBusiness
 $menuresponse
 $officeType
 $suppcountOffice2
 $menuresponseO
 
-#RK choise
-function businessOption {
-    do {
-        newLine(1)
-        Write-Host "do you want to set semi-annual channel and turn off build in preview?"
-        $menuresponseB = read-host "(Y/N)"
-            Switch ($menuresponseB) {
-                "Y" {WindowsUpdateKRMod; $suppCountBusiness = 1 }
-                "N" {$suppCountBusiness = 2}
-            }
-    } 
-    until (1..2 -contains $suppCountBusiness) 
-}
 
-#sub-menù for Office option
-function subOfficeChoise  {
-    $suppOfficeType
-    do {
-        newLine(1)
-        Write-Host "which office do you want to install?"
-        Write-Host "1. Office 2016 VL 64bit `n2. Office 2016 Home & Business 32bit `n3. Office 2019 std-Professional VL `n4. Office 2019 proPlus retail " 
-        $suppOfficeType =Read-Host [inserisci scelta]
-   }
-    until (1..4 -contains $suppOfficeType)
-    return $suppOfficeType
-}
+########### Program Start
 
 #main menu for installation type
+enum InstallationType {
+    school = 1
+    business = 2
+    private = 3
+}   
+
 do {
-    newLine(1)
-    Write-Host "choise the type of installation by the customer:"
-    Write-Host "1. School `n2. Business `n3. Private"
-    $menuresponse = read-host [Inserisci scelta]
-    switch ($menuresponse) {
-        1 { $installationType = 1 }
-        2 { businessOption; $installationType = 2}
-        3 { $installationType = 3 }       
-    }
+    printNewLine(1)
+    Write-Host "Choose installation Type :" +
+        "1. School `n" +
+        "2. Business `n" +
+        "3. Private"
+    [InstallationType]$deployType = read-host [Inserisci scelta]    
+} until (1..3 -contains $deployType)
+
+if ($deployType == [InstallationType]::Business) {
+    chooseWindowsUpdateChannel;
 }
-until (1..3 -contains $menuresponse) 
+
 
 # main menu for office type
+# FIXME: Use enums
 do {
-    newLine(1)
+    printNewLine(1)
     Write-Host "do you want to install Office?"
     $menuresponseO = read-host "(Y/N)"
     Switch ($menuresponseO) {
         "Y" {
-            $officeType = subOfficeChoise
+            $officeType = chooseOfficeReleaseName
             switch ($officeType) {
                 1 { $officeType = 0 }
                 2 { $officeType = 1 }
@@ -146,59 +100,50 @@ do {
             }
             $suppcountOffice = 1
         }
+        
         "N" {$suppCountOffice = 0}
     }  
-}
-until (0..1 -contains $suppCountOffice)
+} until (0..1 -contains $suppCountOffice)
 
-##semi-annual channel and disable preview build WU (Registry path mod)
-function WindowsUpdateKRMod {
-    set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings -Name BranchReadinessLevel -Value 32
-    $registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate";
-
-    if ( !(Test-Path $registryPath) ) { 
-        New-Item -Path $registryPath -Force
-    }
-
-    New-ItemProperty -Path $registryPath -Name "ManagePreviewBuilds" -Value 1 -PropertyType DWORD -Force;
-    New-ItemProperty -Path $registryPath -Name "ManagePreviewBuildsPolicyValue" -Value 0 -PropertyType DWORD -Force;
-}
 
 #Utilities winget
-newLine(1)
-$listOfUtilities = @("7zip.7zip","Google.Chrome","Oracle.JavaRuntimeEnvironment","Adobe.Acrobat.Reader.64-bit")
+printNewLine(1)
+$utilities = @("7zip.7zip","Google.Chrome","Oracle.JavaRuntimeEnvironment","Adobe.Acrobat.Reader.64-bit")
 $businessUtilities = @("CLechasseur.PathCopyCopy","WinDirStat","Microsoft.dotNetFramework")
 $privatesUtilities = @("")
-$toInstall
+$utilities
 
-if ($installationType -eq 2) {
-    $toInstall = $listOfUtilities + $businessUtilities
+# Select utilities 
+if ($deployType -eq 2) {
+    $utilities += $businessUtilities
 }
-elseif ($installationType -eq 3 ) {
-    $toInstall = $listOfUtilities + $privatesUtilities
-}
-else {
-    $toInstall = $listOfUtilities
+elseif ($deployType -eq 3 ) {
+    $utilities += $privatesUtilities
 }
 
-foreach ($utility in $toInstall) {
+foreach ($utility in $utilities) {
     if (winget list --Id $utility) {
         Write-Host "$utility already installed";
-    } 
-    else {
+    } else {
         winget install -e --id $utility
     }
 }
 
-#office installation paths
+# office installation paths
+# FIXME: Static Path specific
 if ($suppCountOffice -eq 1) {
     $mainPath = @("\\ip\path")
-    $officePath = @("Office 2016\Office_2016_64Bit_STD_VolumeLicensing\setup.exe","Office 2016\Home & Businnes Retail x86 x64\HomeBusinessRetail 2016 x86 x64\setup.exe","Office 2019\OfficeProPlus2019ESD\retail\ProPlus2019RetailItalian1\Setup.exe")
+    $officePath = @(
+        "Office 2016\Office_2016_64Bit_STD_VolumeLicensing\setup.exe",
+        "Office 2016\Home & Businnes Retail x86 x64\HomeBusinessRetail 2016 x86 x64\setup.exe",
+        "Office 2019\OfficeProPlus2019ESD\retail\ProPlus2019RetailItalian1\Setup.exe")
     $officeToInstall = $officePath[$officeType]
     start-process -FilePath "$mainPath\$officeToInstall"
 }
 
-#windows update (autoreboot may not work, #damnit)
+# Get windows updates
+# FIXME: AutoReboot doesn't work
+# FIXME: Declare version strings elsewhere
 Find-PackageProvider -Name "NuGet" -AllVersions
 Install-PackageProvider -Name "NuGet" -MinimumVersion 2.8.5.201 -Force;
 
@@ -212,6 +157,6 @@ else {
 Import-Module PSWindowsUpdate
 Get-WindowsUpdate -Install -AcceptAll -RecurseCycle 2 -AutoReboot
 
-#end of the script, ty.
+###end of the script, ty.
 
-#to suggest any change to the script just contact MattMatt19. :)
+# PR welcome.
